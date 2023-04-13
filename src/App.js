022715701356500d6740d196801accf2fb098d92
patryk77k <patryk77.k@gmail.com>
2023-04-13 20:49:11 +0200

    first posts added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header>
+        <a href="" className="">
+          MyBlog
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" />
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.
+        </p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" />
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.
+        </p>
+      </div>
+      <div className="post">
+        <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" />
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.
+        </p>
+      </div>
+    </main>
   );
 }
 
